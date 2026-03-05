534e7bf515d676aa74a5e7927dad69726d8ea62e
Akdemir3055 <thehancafe@gmail.com>
2026-03-05 20:20:42 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:20:28.523Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T17:20:41.964Z
+Commit: Resolve null pointer exception in user service
