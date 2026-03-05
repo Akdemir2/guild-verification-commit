6f0cc8754907d1aed1db7127aa89523ce1450154
Akdemir3055 <thehancafe@gmail.com>
2026-03-05 20:08:46 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:08:20.380Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T17:08:46.189Z
+Commit: Add multi-language support
