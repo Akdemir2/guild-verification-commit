a4a7f654a83659170e203a0505052095d2e2ecf0
Akdemir3055 <thehancafe@gmail.com>
2026-03-05 20:20:45 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:20:25.120Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T17:20:44.700Z
+Commit: Fix memory leak in event handlers
