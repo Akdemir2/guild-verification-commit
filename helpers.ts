3463b38da69a0ca4e9322025c02019f7175611f9
Akdemir3055 <thehancafe@gmail.com>
2026-03-05 20:20:29 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:20:13.828Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T17:20:28.523Z
+Commit: Patch API endpoint error handling
