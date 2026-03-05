c8d35a36e532cc098535f2d0d5eb9178b17e4d2c
Akdemir3055 <thehancafe@gmail.com>
2026-03-05 20:24:12 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:20:50.540Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T17:24:11.443Z
+Commit: Resolve timezone issues in date handling
