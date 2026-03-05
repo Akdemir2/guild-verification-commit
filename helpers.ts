c684063199dda3e7ab3db639f2c800667fa74f51
Akdemir3055 <thehancafe@gmail.com>
2026-03-05 20:25:02 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:24:53.373Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T17:25:01.564Z
+Commit: Patch security vulnerability in authentication
