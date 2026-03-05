22e752bbff7338cb2b0c35d08fd6342c509254bc
Akdemir3055 <thehancafe@gmail.com>
2026-03-05 20:23:35 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:23:29.052Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T17:23:34.947Z
+Commit: Resolve null pointer exception in user service
