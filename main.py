2f743c49dc0f29e0b979416db29e0ad1b4780d6c
Akdemir3055 <thehancafe@gmail.com>
2026-03-05 20:20:46 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:19:47.843Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T17:20:46.140Z
+Commit: Introduce real-time notifications
